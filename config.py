58ef5856542c38159489df4a72db10d55d958c10
deepcode-ivan <48474286+deepcode-ivan@users.noreply.github.com>
2019-07-02 21:47:19 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 # bot config
-bot_token = "475994989:AAG2v7JHPvEiSE8taAoohftpA-wdxWgwkZs"
+bot_token = "---"
 
 # mysql config
 mysql_host='localhost'
 mysql_database='smart_home'
 mysql_username='root'
 mysql_password='1234'
-# mysql_password='7891'
 db_config ={'host': mysql_host,
             'database': mysql_database,
             'user': mysql_username,
@@ -23,5 +22,5 @@ mqtt_username="smart_home"
 mqtt_password="fbsjiouu"
 
 # SpeechKit Cloud config
-YANDEX_API_KEY = '23fc61fb-f713-432e-aa58-1a6637fa7a56'
+YANDEX_API_KEY = '---'
     #'b0b7b360-ea10-4335-aa1e-8fe9eea6d033'
